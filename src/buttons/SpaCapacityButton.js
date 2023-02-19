f66a47f3a334ae92839ad1709ccccca17fa4a01c
Cristian Rodriguez <chrodriguez34@gmail.com>
2023-02-19 22:29:27 +0100

    minor edits and refactors

---
+++ src/buttons/SpaCapacityButton.js
@@ -0,0 +1,21 @@
+import GroupsIcon from '@mui/icons-material/Groups';
+import {
+    Button,
+    Stack,
+    Typography }
+    from '@mui/material';
+
+export default function SpaCapacityButton({capacity}) {
+    return (
+        <Button 
+            sx={{width: "-webkit-fill-available"}} 
+            size="large" 
+            variant="outlined"
+            color="inherit">
+            <Stack direction="column" alignItems="center" justifyContent={"center"}>
+                <GroupsIcon />
+                <Typography>Spa Capacity: {capacity}</Typography>
+            </Stack>
+        </Button>
+    )
+}
